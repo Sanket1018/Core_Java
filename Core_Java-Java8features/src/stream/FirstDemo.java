9e1276ad80a26b60bbcb4b84c91571de34c3cd8c
Sanket Khokade <sanketkhokadesk@gmail.com>
2026-02-05 17:42:47 +0530

    adding stream api code

---
+++ Core_Java-Java8features/src/stream/FirstDemo.java
@@ -0,0 +1,41 @@
+package stream;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class FirstDemo {
+
+	public static void main(String[] args) {
+		// Apporoch1
+		Stream<Integer> stream1 = Stream.of(1,2,3,4);
+		
+		//Apporoch2 
+		ArrayList<String> names = new ArrayList<>();	
+		names.add("Sanket");
+		names.add("Rohan");
+		names.add("Akash");
+		
+		Stream<String> stream2 = names.stream();
+		
+		User u1 = new User("Sanket",23);
+		User u2  = new User("Rohan",21);
+		User u3 = new User("Akash",24);
+		
+	
+		Stream<User> stream = Stream.of(u1,u2,u3);
+		stream.filter(u-> u.age > 21).forEach(i-> System.out.println(i));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ Core_Java-Java8features/src/stream/User.java
@@ -0,0 +1,17 @@
+package stream;
+
+public class User {
+	String name;
+	int age;
+	
+	public User(String name,int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + "]";
+	}
+	
+}
